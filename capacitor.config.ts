7cdaaed808ba8467403249b8aed4310e18087180
HectorPaulo <adrian.paulo.vaz@gmail.com>
2025-09-18 15:59:45 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CenaduriaSonic',
+  webDir: 'www'
+};
+
+export default config;
